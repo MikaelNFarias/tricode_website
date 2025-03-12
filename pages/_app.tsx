5042075202d2246648d0a1b47b0b30028d1f05dd
Mikael Farias <mikael.n.farias@gmail.com>
2025-03-12 18:12:35 -0300

    feat: adjust error on imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import packageJson from '../package.json';
 import GlobalStyle from '../styles/globalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  //const version = `${packageJson.name.toUpperCase()}${packageJson.version}`;
-
   return (
     <>
       <GlobalStyle />
